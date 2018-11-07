a86de9e583b2f5f45f14151cf2a1c0141c3a0168
Mohammad Hossein Amri <amrimh@outlook.com>
2018-11-07 18:25:18 +0800

    Update Vector.cs
    add more comment

--- Question2/Vector.cs
+++ Question2/Vector.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace Ericson.AmriAnswers.Question2
 {
+    // just a note, since i finished my highschool in other than english, so term "phase" wasn't known to me. 
+    // with google help also, still i couldn't find a definition for the term.
+    // a vector can be sum in two way, by x,y and magnitude and degree between them. i tried many different keyword but still i couldn't find anything regard to vector and phase. 
+    // so in this solution i only cover the sum based on magnitude and degree between the magnitude, hope you ignore the lack if there is any!
     public class Vector
     {
         private double _degree;
@@ -25,11 +29,14 @@ namespace Ericson.AmriAnswers.Question2
 
         public static Vector operator +(Vector u, Vector v)
         {
-            //https://www.youtube.com/watch?v=8YsMNFWvgpk
+            // in a normal program i woudlnt clutter a function with a lot of delegate. every of these delegates should go into a helper or relavant class 
+            // but usually i follow the "rule of 3". a function should be needed more than 3 times to make it available inside a class. 
+            // so here i only need it once and only in this example, so i used the "in-method functions". performance wise it doesn't affect since IL will convert theme to anonymous function on build. it only keep the 
+            // class API cleaner
 
             var convertToRadiant = new Func<double, double>(d => d * (Math.PI / 180.0));
             var convertToDegree = new Func<double, double>(r => r * (180.0 / Math.PI));
-
+            
             var degreeBetweenTwoLine = new Func<double, double, double>((d1, d2) =>
             {
                 //https://www.youtube.com/watch?v=ILzOxUNXuSs
@@ -42,8 +49,8 @@ namespace Ericson.AmriAnswers.Question2
 
                 return degree;
             });
-
-            //c^2=a^2+b^2-2abCos0
+            
+            //c^2=a^2+b^2-2abCos0  https://www.youtube.com/watch?v=8YsMNFWvgpk
             var c = new Func<double, double, double, double>((a, b, degree) =>
                 Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2) - 2 * a * b * Math.Cos(convertToRadiant(degree))));
 
